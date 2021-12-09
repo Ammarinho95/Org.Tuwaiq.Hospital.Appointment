bb5a9c465fa0e561b2bc6f14b572e9215b9a2f5c
Amar <suliseg@hotmail.com>
2021-12-09 16:34:27 +0300

    Done designing activity_book_appointment.xml

--- app/src/main/java/com/example/orgtuwaiqhospitalappointment/view/Home/HomeActivity.kt
+++ app/src/main/java/com/example/orgtuwaiqhospitalappointment/view/Home/HomeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.orgtuwaiqhospitalappointment.view.Home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.orgtuwaiqhospitalappointment.R
+import com.example.orgtuwaiqhospitalappointment.view.BookAppointment.BookAppointmentActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        Handler().postDelayed({
+            var i= Intent(this,BookAppointmentActivity::class.java)
+            startActivity(i)
+
+        },3000)
+
     }
 }
