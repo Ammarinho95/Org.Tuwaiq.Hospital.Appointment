788054a7f47a28573b869d9c47c640a275444b79
Amar <suliseg@hotmail.com>
2021-12-11 19:50:58 +0300

    Finished designing ClinicNamesActivity.kt and started working on AppointmentDetailsActivity.kt

---
+++ app/src/main/java/com/example/orgtuwaiqhospitalappointment/view/AppointmentDetails/AppointmentDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.orgtuwaiqhospitalappointment.view.AppointmentDetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orgtuwaiqhospitalappointment.R
+
+class AppointmentDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointment_details)
+    }
+}
