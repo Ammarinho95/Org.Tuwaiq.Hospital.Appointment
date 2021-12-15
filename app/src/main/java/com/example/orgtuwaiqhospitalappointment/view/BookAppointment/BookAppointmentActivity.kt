89b823034bd59b6ca29c3f7e83b6d875389dd5bb
Amar <suliseg@hotmail.com>
2021-12-15 12:20:17 +0300

    started working on Book appointment page

--- app/src/main/java/com/example/orgtuwaiqhospitalappointment/view/BookAppointment/BookAppointmentActivity.kt
+++ app/src/main/java/com/example/orgtuwaiqhospitalappointment/view/BookAppointment/BookAppointmentActivity.kt
@@ -1,14 +1,18 @@
 package com.example.orgtuwaiqhospitalappointment.view.BookAppointment
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.RadioButton
+import android.widget.TextView
 import com.example.orgtuwaiqhospitalappointment.R
 import com.example.orgtuwaiqhospitalappointment.view.row.rowActivity
 import com.google.android.material.textfield.TextInputEditText
+import java.util.*
 
 class BookAppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,28 +23,29 @@ class BookAppointmentActivity : AppCompatActivity() {
         var virtual = findViewById<RadioButton>(R.id.radioButtonVirtual)
         var PatientName = findViewById<TextInputEditText>(R.id.textPatientName1)
         var ClincName = findViewById<TextInputEditText>(R.id.textClincName1)
-        var HealthCareCenter = findViewById<TextInputEditText>(R.id.textHealthCenter1)
-        var btnBook= findViewById<Button>(R.id.buttonBook)
+        var btnBook = findViewById<Button>(R.id.buttonBook)
+        var selectDate = findViewById<TextView>(R.id.textViewPickdate)
+        var showDate= findViewById<TextView>(R.id.textViewshowDate)
+        var selectDateIcon = findViewById<ImageView>(R.id.imageViewCalender)
 
+        selectDateIcon.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
 
+            var dialogDatePickerDialog = DatePickerDialog(
+                this,
+                DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                    showDate.text = "$day/$month/$year"
+                },
+                year,
+                month,
+                day
+            )
+            dialogDatePickerDialog.show()
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-        Handler().postDelayed({
-            var i = Intent(this, rowActivity::class.java)
-            startActivity(i)
-
-        }, 3000)
 
 
     }
